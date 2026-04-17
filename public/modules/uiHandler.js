93dfed114b7736eba5212584747939a1bab67b08
“Ro <halfhide@gmail.com>
2026-04-17 15:00:19 +0200

    fix

--- public/modules/uiHandler.js
+++ public/modules/uiHandler.js
@@ -155,8 +155,7 @@ export function applyFilters() {
 
     state.currentFilteredData = state.allArtists.filter(artist => {
         const matchesSearch = (artist.firstName + ' ' + artist.lastName + artist.artistName + artist.email).toLowerCase().includes(searchTerm)
-            || artist.notities?.toLowerCase().includes(searchTerm)
-            || artist.opmerkingen?.toLowerCase().includes(searchTerm);
+            || (artist.notes || '').toLowerCase().includes(searchTerm);
         let matchesRegion = regionFilter === 'all' || (regionFilter === 'Den Haag' && artist.regionDH) || (regionFilter === 'Rotterdam' && artist.regionRdam);
         const matchesType = typeFilter === 'all' || artist.type === typeFilter;
         let matchesBookable = bookableFilter === 'all' || (bookableFilter === 'ja' && artist.bookable) || (bookableFilter === 'nee' && !artist.bookable);
